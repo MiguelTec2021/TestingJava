6209a8dc12e770b7c9c7836e1086b1afb93b4c2e
Miguel Ortiz <tecnicohernandez2018@gmail.com>
2021-06-14 17:55:26 -0500

    Organizacion de test con JUnit

--- src/main/java/com/uttl/javatest/util/StringUtil.java
+++ src/main/java/com/uttl/javatest/util/StringUtil.java
@@ -3,8 +3,13 @@ package com.uttl.javatest.util;
 public class StringUtil {
 
     public static String repeat(String str, int times){
-        
+
+        if (times < 0 ){
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
         String result = "";
+
         for (int i = 0; i < times; i++) {
             result += str;
         }
