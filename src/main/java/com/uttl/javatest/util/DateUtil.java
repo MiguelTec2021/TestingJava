d60689999eb0467aa483fd6e9885b87ac8497803
Miguel Ortiz <tecnicohernandez2018@gmail.com>
2021-06-16 14:29:58 -0500

    Inicio de TDD

---
+++ src/main/java/com/uttl/javatest/util/DateUtil.java
@@ -0,0 +1,11 @@
+package com.uttl.javatest.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+
+
+        return true;
+    }
+
+}
---
+++ src/test/java/com/uttl/javatest/util/DateUtilShould.java
@@ -0,0 +1,7 @@
+package com.uttl.javatest.util;
+
+import static org.junit.Assert.*;
+
+public class DateUtilShould {
+
+}
