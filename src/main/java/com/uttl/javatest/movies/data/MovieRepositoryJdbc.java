493bfc53401e0a285f97dae8adc54a081444d497
Miguel Ortiz <tecnicohernandez2018@gmail.com>
2021-06-18 00:59:30 -0500

    Creación de la base de datos y test de integración

---
+++ src/main/java/com/uttl/javatest/movies/data/MovieRepositoryJdbc.java
@@ -0,0 +1,43 @@
+package com.uttl.javatest.movies.data;
+
+import com.uttl.javatest.movies.model.Genre;
+import com.uttl.javatest.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class MovieRepositoryJdbc implements MovieRepository {
+     private JdbcTemplate  jdbcTemplate;
+
+    public MovieRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+
+        return jdbcTemplate.query("select * from movies", movieMappert);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+
+    private  static RowMapper<Movie> movieMappert = (resultSet, i) ->
+            new Movie(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getInt("minutes"),
+                Genre.valueOf(resultSet.getString("genre")));
+}
